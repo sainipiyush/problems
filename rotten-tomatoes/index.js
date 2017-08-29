957feee1f630791a042c2b5229f09a5f9f3ed7a8
EPIYSAI <piyush.saini@ericsson.com>
2017-08-28 19:30:03 -0700

    getting image url from json

--- rotten-tomatoes/index.js
+++ rotten-tomatoes/index.js
@@ -2,8 +2,27 @@ const express = require('express')
 const app = express()  
 const port = 8080
 
-app.get('/movie/:movieId', (request, response) => {  
-  response.send('Hello from Express!')
+
+app.param('movieId', function(request, response, next, movieId){
+  
+    if(movieId!="771373077") {
+		response.end('Invalid movie id');
+	}
+});
+
+app.get('/movie/:movieId', (request, response) => {
+	
+	var fs = require('fs');
+
+	fs.readFile('take-home-movie.json', 'utf8', function (err, data) {
+		if (err) throw err; // we'll not consider error handling for now
+		var jsonData = JSON.parse(data);
+				
+	//	console.log("image url: " + jsonData.data.relationships.images.links.related);
+		response.send("image url: " + jsonData.data.relationships.images.links.related)
+	});
+
+	
 })
 
 app.listen(port, (err) => {  
